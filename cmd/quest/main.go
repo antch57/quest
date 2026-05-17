31eca3bf936d5cf3122920df9e44b0367a8a7b88
ant <38507580+antch57@users.noreply.github.com>
2026-05-17 00:56:15 -0600

    fix: quest-13 - new release workflow, add versioning

--- cmd/quest/main.go
+++ cmd/quest/main.go
@@ -7,11 +7,14 @@ import (
 	"github.com/urfave/cli/v3"
 )
 
+var version = "dev"
+
 // App returns the root CLI command for quest.
 func App() *cli.Command {
 	return &cli.Command{
-		Name:  "quest",
-		Usage: "get shit done..",
+		Name:    "quest",
+		Usage:   "a cli tool to help you manage your quests",
+		Version: version,
 		Commands: []*cli.Command{
 			log.Command(),
 			jamz.Command(),
